73b9e4b816448f4b2c80c28485a13bd1da378c5c
dofany <kimdohwan17@gmail.com>
2019-06-04 11:54:39 +0900

    enitial commit

---
+++ src/main/java/net/slipp/HelloWorldController.java
@@ -0,0 +1,13 @@
+package net.slipp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@GetMapping("/helloworld")
+	public String helloworld() {
+		return "Hello World!";
+	}
+
+}
